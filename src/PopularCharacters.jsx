535a328a80e54b8c9bf337a92a7317ef251063b5
Marty Powell <jpowel25@gmail.com>
2019-12-10 20:12:45 -0500

    added basic popular character component

---
+++ src/PopularCharacters.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const PopularCharacters = props => {
+  return <div>Component stuff goes here.</div>;
+};
+
+PopularCharacters.propTypes = {};
+
+export default PopularCharacters;
