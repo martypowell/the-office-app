ce1b7fc14942601f69ed5bc77f653004e6de745d
Marty Powell <jpowel25@gmail.com>
2019-12-10 21:45:40 -0500

    added test to validate popular services

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const popularCharacter1 = getByText(/Michael Scott/i);
+  const popularCharacter2 = getByText(/Dwight Schrute/i);
+  const popularCharacter3 = getByText(/Jim Halpert/i);
+  expect(popularCharacter1).toBeInTheDocument();
+  expect(popularCharacter2).toBeInTheDocument();
+  expect(popularCharacter3).toBeInTheDocument();
 });
