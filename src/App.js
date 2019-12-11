9737b03d3229fef71c043740eda722530e7f4c82
Marty Powell <jpowel25@gmail.com>
2019-12-10 21:45:59 -0500

    implemented changes to ensure test passed

--- src/App.js
+++ src/App.js
@@ -12,6 +12,24 @@ const characters = [
     giphy:
       '<iframe src="https://giphy.com/embed/GQ9InpECkHgJO" width="480" height="278" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/romy-the-office-michael-scott-youre-worst-GQ9InpECkHgJO">via GIPHY</a></p>',
     image: "//gettoitmovement.com/wp-content/uploads/2017/03/michael-scott.jpg"
+  },
+  {
+    name: "Dwight Schrute",
+    title: "Assistant to the Regional Manager",
+    description:
+      "I miss the feeling of knowing you did a good job because someone gives you proof of it. 'Sir, you're awesome, let me give you a plaque! What? A whole year has gone by? You need more proof? Here is a certificate.' They stopped making plaques that year.",
+    giphy:
+      '<iframe src="https://giphy.com/embed/122pLlowwMS5aM" width="480" height="271" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/weird-the-office-unicorn-122pLlowwMS5aM">via GIPHY</a></p>',
+    image: "//assets.rbl.ms/10672280/origin.jpg"
+  },
+  {
+    name: "Jim Halpert",
+    title: "Number 2",
+    description:
+      "I miss the feeling of knowing you did a good job because someone gives you proof of it. 'Sir, you're awesome, let me give you a plaque! What? A whole year has gone by? You need more proof? Here is a certificate.' They stopped making plaques that year.",
+    giphy:
+      '<iframe src="https://giphy.com/embed/qTEwof5AZerwQ" width="480" height="304" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/the-office-prank-john-krasinski-qTEwof5AZerwQ">via GIPHY</a></p>',
+    image: "//assets.rbl.ms/10672280/origin.jpg"
   }
 ];
 
