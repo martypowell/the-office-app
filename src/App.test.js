cf79d8c3232445f61619a59247c6f159db8361ec
Marty Powell <jpowel25@gmail.com>
2019-12-11 20:06:56 -0500

    adjusted tests to handle async

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,37 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, waitForElement } from "@testing-library/react";
 import App from "./App";
+import axios from "axios";
 
-test("renders learn react link", () => {
+jest.mock("axios");
+
+test("renders learn react link", async () => {
+  const mockCharacters = [
+    {
+      name: "Michael Scott",
+      title: "Regional Manager",
+      image: "url-goes-here"
+    },
+    {
+      name: "Dwight Schrute",
+      title: "Assistant to the Regional Manager",
+      image: "url-goes-here"
+    },
+    { name: "Jim Halpert", title: "Number 2", image: "url-goes-here" }
+  ];
+  const resp = { data: mockCharacters };
+  axios.get.mockResolvedValue(resp);
   const { getByText } = render(<App />);
-  const popularCharacter1 = getByText(/Michael Scott/i);
-  const popularCharacter2 = getByText(/Dwight Schrute/i);
-  const popularCharacter3 = getByText(/Jim Halpert/i);
+
+  const popularCharacter1 = await waitForElement(() =>
+    getByText(/Michael Scott/i)
+  );
+  const popularCharacter2 = await waitForElement(() =>
+    getByText(/Dwight Schrute/i)
+  );
+  const popularCharacter3 = await waitForElement(() =>
+    getByText(/Jim Halpert/i)
+  );
   expect(popularCharacter1).toBeInTheDocument();
   expect(popularCharacter2).toBeInTheDocument();
   expect(popularCharacter3).toBeInTheDocument();
