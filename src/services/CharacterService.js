1771f114472aa0ea813183e3f716e5363ae26bc1
Marty Powell <jpowel25@gmail.com>
2019-12-11 19:51:31 -0500

    implemented popular character service method

---
+++ src/services/CharacterService.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+/**
+ * Get the most popular office characters
+ * @param {number} limit number of characters to return, if zero all characters are returned
+ * @returns {array} list of popular characters
+ */
+const GetPopularCharacters = (limit = 0) =>
+  axios
+    .get("../data/character.json")
+    .then(resp => resp.data)
+    .then(characters => (limit ? characters.slice(0, limit) : characters));
+
+export { GetPopularCharacters };
