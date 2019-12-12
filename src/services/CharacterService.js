5bb47879f84a93656aadd8b5ce54cd377cc02c5e
Marty Powell <jpowel25@gmail.com>
2019-12-11 20:11:07 -0500

    added fake lag to display loading message

--- src/services/CharacterService.js
+++ src/services/CharacterService.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+function sleeper(ms) {
+  return function(x) {
+    return new Promise(resolve => setTimeout(() => resolve(x), ms));
+  };
+}
+
 /**
  * Get the most popular office characters
  * @param {number} limit number of characters to return, if zero all characters are returned
@@ -8,6 +14,7 @@ import axios from "axios";
 const GetPopularCharacters = (limit = 0) =>
   axios
     .get("../data/character.json")
+    .then(sleeper(1000))
     .then(({ data }) => data)
     .then(characters => (limit ? characters.slice(0, limit) : characters));
 
