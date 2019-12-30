8af7d81f71f292871a5639ccafa0c66b34839ba2
Marty Powell <jpowel25@gmail.com>
2019-12-29 20:19:55 -0500

    added favorite character component

---
+++ src/components/FavoriteCharcter.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const FavoriteCharacter = props => {
+  return <div>Component stuff goes here.</div>;
+};
+
+FavoriteCharacter.propTypes = {};
+
+export default FavoriteCharacter;
